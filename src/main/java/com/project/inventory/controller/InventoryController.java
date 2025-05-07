8c41adab9e20fc91931bcf38fe0d7a1a52d31dde
dhruvajyoti1997 <dhruvajyotipramanik080@gmail.com>
2025-05-07 23:39:08 +0530

    formatted the code

--- src/main/java/com/project/inventory/controller/InventoryController.java
+++ src/main/java/com/project/inventory/controller/InventoryController.java
@@ -3,13 +3,10 @@ package com.project.inventory.controller;
 
 import com.project.inventory.model.Details;
 import com.project.inventory.service.InventoryService;
-import com.project.inventory.serviceImpl.InventoryServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,45 +23,40 @@ public class InventoryController {
     @Autowired // if we use this annotation . it means we are creating the bean directly
     private InventoryService inventoryService;
 
-    // /getAllProducts (GET)
-    // /inventory (POST)
-    // /inventoryById (PUT)
-    // /deleteInventoryById (DELETE)
-    // /getProductById (GraphQl)
-
     //response entity is a part of spring framework , which will help us to show the output of the response and
     // corresponding status http code.
     @PostMapping("/")
-    public ResponseEntity<List<Details>>SaveProductDetails(@RequestBody Details details){
-
+    public ResponseEntity<List<Details>> SaveProductDetails(@RequestBody Details details) {
         logger.info("inside the post method");
         List<Details> productDetailList = inventoryService.saveListOfProductDetails(details);
         return new ResponseEntity<>(productDetailList, HttpStatus.CREATED);
-
-
     }
+
     @GetMapping("/getAllProducts")
-    public  ResponseEntity<List<Details>> getListOfProducts(){
+    public ResponseEntity<List<Details>> getListOfProducts() {
         List<Details> getProductsList = inventoryService.retriveListOfProducts();
         return new ResponseEntity<>(getProductsList, HttpStatus.OK);
     }
+
     @GetMapping("/getProductById/{id}")
-   public ResponseEntity<Optional<Details>> getProductDetailsById(@PathVariable("id") Long id){
+    public ResponseEntity<Optional<Details>> getProductDetailsById(@PathVariable("id") Long id) {
         Optional<Details> productDetails = inventoryService.getProductById(id);
         return ResponseEntity.ok(productDetails);
-   }
+    }
+
     @DeleteMapping("/deleteInventory/{id}")
-   public ResponseEntity<String> deleteProductDetails(@PathVariable("id") Long id){
-       Optional<Details> retriveProductDetails = inventoryService.getProductById(id);
-       if(retriveProductDetails.isPresent()){
-           inventoryService.deleteProduct(id);
-           return  ResponseEntity.ok("Product Deleted");
-       }
-        return new ResponseEntity<>("Product Not Present In Database",HttpStatus.NOT_FOUND);
-   }
+    public ResponseEntity<String> deleteProductDetails(@PathVariable("id") Long id) {
+        Optional<Details> retriveProductDetails = inventoryService.getProductById(id);
+        if ( retriveProductDetails.isPresent() ) {
+            inventoryService.deleteProduct(id);
+            return ResponseEntity.ok("Product Deleted");
+        }
+        return new ResponseEntity<>("Product Not Present In Database", HttpStatus.NOT_FOUND);
+    }
+
     @PutMapping("/updateProductDetails")
-   public ResponseEntity<Details> updateProductDetails(@RequestBody Details details){
+    public ResponseEntity<Details> updateProductDetails(@RequestBody Details details) {
         Details updateDetails = inventoryService.updateProductDetails(details);
         return new ResponseEntity<Details>(updateDetails, HttpStatus.OK);
-   }
+    }
 }
