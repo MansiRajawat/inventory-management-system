708e4f124f888118cb81b668c1cba9d1507eca73
Mansi112 <rajawatmansi042@gmail.com>
2025-05-12 23:17:47 +0530

    POJO changes

--- src/main/java/com/project/inventory/model/ProductDetails.java
+++ src/main/java/com/project/inventory/model/ProductDetails.java
@@ -1,5 +1,6 @@
 package com.project.inventory.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -12,9 +13,12 @@ import lombok.Data;
 @Table(name = "product")
 public class ProductDetails {
     @Id
-  //  @NotNull
+    @Column(name = "productId")
     private Long productId;
+    @Column(name = "productName")
     private String productName;
+    @Column(name = "productPrice")
     private double productPrice;
+    @Column(name = "productCount")
     private int productCount;
 }
