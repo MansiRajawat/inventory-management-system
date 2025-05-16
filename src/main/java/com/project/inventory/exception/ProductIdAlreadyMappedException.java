9243f590e6ed830defe1f75e21ef9c7c84833297
MansiRajawat <rajawatmansi042@gmail.com>
2025-05-16 23:06:11 +0530

    Delete src directory

--- src/main/java/com/project/inventory/exception/ProductIdAlreadyMappedException.java
+++
@@ -1,7 +0,0 @@
-package com.project.inventory.exception;
-
-public class ProductIdAlreadyMappedException extends RuntimeException{
-    public ProductIdAlreadyMappedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/project/inventory/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.project.inventory.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
